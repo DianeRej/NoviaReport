65af720c97b385f7fe49e6112fcb1859ff08b500
Diane REJA <diane.reja@gmail.com>
2023-03-21 09:44:31 +0100

    MàJ IDal

--- NoviaReport/Models/IDal.cs
+++ NoviaReport/Models/IDal.cs
@@ -10,6 +10,13 @@ namespace NoviaReport.Models
         int CreateProfile(string firstName ,string lastName);
 
         void UpdateProfile(int id, string firstName ,string lastName);
+        void UpdateProfile(Profile profile);
+        public int CreateUser(string login, string password);
+        public int CreateUser(User user);
+        public int CreateContact(string personalMail, string personalPhone, string proMail,
+           string proPhone, Adress adress, int adressId);
+        public int CreateAdress(int num, string street, int postalCode, string city);
+
 
         List<Profile> GetAllProfiles();
     }
